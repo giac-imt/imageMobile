3dcf621cab4f9b8cd0e94e4233f965330535cac4
giac-imt <32895137+giac-imt@users.noreply.github.com>
2019-02-04 17:41:56 +0100

    changement API pour téléphoone + ajout d'un bouton pour test connexion front/back

--- FrontImageMobile/app/src/main/java/imt/fr/frontimagemobile/PhotoActivity.java
+++ FrontImageMobile/app/src/main/java/imt/fr/frontimagemobile/PhotoActivity.java
@@ -1,16 +1,30 @@
 package imt.fr.frontimagemobile;
 
+import android.app.VoiceInteractor;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
 
 public class PhotoActivity extends AppCompatActivity {
 
     ImageView img;
+
+    Button btn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -18,6 +32,14 @@ public class PhotoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_photo);
 
         img = findViewById(R.id.image_camera);
+        btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                testImageRequest();
+            }
+        });
 
         Intent intent = getIntent();
         if(intent != null) {
@@ -26,5 +48,26 @@ public class PhotoActivity extends AppCompatActivity {
             Bitmap imageBitmap = (Bitmap) intentBundle.get("image");
             img.setImageBitmap(imageBitmap);
         }
+    }
+
+    private void testImageRequest(){
+        String url = "http://192.168.1.33";
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonArrayRequest jsonRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d("tagggg", "réponse OK");
+                    }
+                }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.d("tagggg", "Réponse KO");
+                }
+        });
+        queue.add(jsonRequest);
     }
 }
