7e28d9c510e3ef7b6bb0ba61d7e4b19674b1fa11
giac-imt <32895137+giac-imt@users.noreply.github.com>
2019-02-15 13:05:08 +0100

    Modification de la requete get->post pour envoyer url en body

--- FrontImageMobile/app/src/main/java/imt/fr/frontimagemobile/ZipActivity.java
+++ FrontImageMobile/app/src/main/java/imt/fr/frontimagemobile/ZipActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,8 +15,17 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ZipActivity extends AppCompatActivity {
 
@@ -45,37 +55,49 @@ public class ZipActivity extends AppCompatActivity {
         btn_envoyer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), R.string.zip_activity_send_zip, Toast.LENGTH_SHORT).show();
-                remplacerDataset();
+                if(isValidUrl()) {
+                    Toast.makeText(getApplicationContext(), R.string.zip_activity_send_zip, Toast.LENGTH_SHORT).show();
+                    remplacerDataset();
+                } else {
+                    Toast.makeText(getApplicationContext(), R.string.zip_activity_url_error, Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
 
     private void remplacerDataset(){
-        String url = "http://192.168.1.33:8000/zip/" + editTextUrl.getText();
+        Map<String,String> params = new HashMap<>();
+        params.put("url", editTextUrl.getText().toString());
+
+        JSONObject jsonObject = new JSONObject(params);
+
+        String url = "http://192.168.1.33:8000/zip/";
         RequestQueue queue = Volley.newRequestQueue(this);
-        StringRequest stringRequest = new StringRequest(
-                Request.Method.GET,
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.POST,
                 url,
-                new Response.Listener<String>() {
+                jsonObject,
+                new Response.Listener<JSONObject>() {
                     @Override
-                    public void onResponse(String response) {
-                        Log.d(this.getClass().getSimpleName() + " GET", "zip OK");
+                    public void onResponse(JSONObject response) {
+                        Log.d(this.getClass().getSimpleName() + " POST", "zip OK");
                         Toast.makeText(getApplicationContext(), R.string.zip_activity_dataset_replaced, Toast.LENGTH_SHORT).show();
                     }
                 }, new Response.ErrorListener() {
                 @Override
                 public void onErrorResponse(VolleyError error) {
-                    Log.d(this.getClass().getSimpleName() + " GET", "zip KO : " + error.getMessage() + error.getCause());
+                    Log.d(this.getClass().getSimpleName() + " POST", "zip KO : " + error.getMessage() + error.getCause());
                     Toast.makeText(getApplicationContext(), R.string.zip_activity_dataset_error, Toast.LENGTH_SHORT).show();
-
                 }
             });
-
-        // Ajout d'un timeout long pour ne pas avoir plusieurs GET
-        stringRequest.setRetryPolicy(new DefaultRetryPolicy(40000,
+        jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(30000,
                 DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
                 DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
-        queue.add(stringRequest);
+        queue.add(jsonObjectRequest);
+    }
+
+    // validating email id
+    private boolean isValidUrl() {
+        return Patterns.WEB_URL.matcher(editTextUrl.getText()).matches();
     }
 }
